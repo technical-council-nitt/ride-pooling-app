8322b8634a6a9a19c748f9f9f7b105f9f8acc7cf
Arunesh Unnithan <aruneshunnithan@gmail.com>
2025-01-17 19:50:50 +0530

    Initial commit

---
+++ src/pages/home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-red-500 bg-red-500">Contact Page</h1>
+            <p>Feel free to reach out to us.</p>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/login.js
@@ -0,0 +1,10 @@
+const Login = () => {
+    return (
+        <div>
+            <h1>Contact Page</h1>
+            <p>Feel free to reach out to us.</p>
+        </div>
+    );
+};
+
+export default Login;
